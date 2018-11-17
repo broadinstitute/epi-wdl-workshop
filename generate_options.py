15546a6741007e9b12d57d95300c09a121a59ecf
Denis Loginov <denis@broadinstitute.org>
2018-11-17 02:54:14 -0500

    Add default runtime attributes to options.json

--- generate_options.py
+++ generate_options.py
@@ -7,19 +7,38 @@ project = argv[1]
 bucket = argv[2]
 key_file = argv[3]
 
-options_file = 'options.json'
+OPTIONS_FILE = 'options.json'
+
+DEFAULT_CPU = 2
+DEFAULT_MEMORY = '2G'
+DEFAULT_DISK = 'local-disk 25 HDD'
+ZONES = [
+  'us-east1-b', 'us-east1-c', 'us-east1-d',
+  'us-central1-a', 'us-central1-b', 'us-central1-c', 'us-central1-f',
+  'us-west1-a', 'us-west1-b', 'us-west1-c'
+]
+MAX_PREEMPTIBLE = 3
+MAX_RETRIES = 1
 
 def main():
   with open(key_file, 'r') as f:
     key = json.dumps(json.load(f))
 
-  with open(options_file, 'w') as out:
+  with open(OPTIONS_FILE, 'w') as out:
     options = {
       'google_project': project,
       'jes_gcs_root': 'gs://' + bucket,
       'user_service_account_json': key,
+      'default_runtime_attributes': {
+        'cpu': DEFAULT_CPU,
+        'memory': DEFAULT_MEMORY,
+        'disks': DEFAULT_DISK,
+        'zones': ZONES,
+        'preemptible': MAX_PREEMPTIBLE,
+        'maxRetries': MAX_RETRIES,
+      },
     }
     json.dump(options, out, indent=2)
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     main()
