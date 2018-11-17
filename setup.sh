9314fa2de8525a2729e2b204edb582afd9becfe1
Denis Loginov <denis@broadinstitute.org>
2018-11-17 00:58:08 -0500

    Refactor setup.sh

--- setup.sh
+++ setup.sh
@@ -2,21 +2,13 @@
 
 set -e
 
+### Set up Google project
+
 get_project() {
   gcloud config list --format 'value(core.project)'
 }
 
 PROJECT=${1:-"$(get_project)"}
-
-user_name() {
-  gcloud config list --format 'value(core.account.split("@").slice(0))'
-}
-
-SERVICE_ACCOUNT=${2:-"cromwell-$(user_name)"}
-
-### Set up Google Project
-
-gcloud config set project ${PROJECT}
 
 enable_api() {
   gcloud services enable "$1"
@@ -25,23 +17,22 @@ enable_api() {
 enable_api compute
 enable_api genomics
 
-### Create Cromwell executions bucket if it doesn't exist
-
-BUCKET=${3:-"${PROJECT}-cromwell-executions"}
-REGION=${4:-"us-east1"}
-
-gsutil mb -l "${REGION}" "gs://${BUCKET}" 2>/dev/null || true
-
 ### Generate Cromwell Pet Service Account with the necessary roles and a key
 
 # Create SA if it doesn't exist
+
+username() {
+  gcloud config list --format 'value(core.account.split("@").slice(0))'
+}
+
+SERVICE_ACCOUNT=${2:-"cromwell-$(username)"}
 
 SERVICE_ACCOUNT_EMAIL="${SERVICE_ACCOUNT}@${PROJECT}.iam.gserviceaccount.com"
 
 gcloud iam service-accounts create "${SERVICE_ACCOUNT}" \
   --display-name "${SERVICE_ACCOUNT}" 2>/dev/null || true
 
-# Add roles
+# Add roles required by CaaS
 
 add_role() {
   gcloud projects add-iam-policy-binding "${PROJECT}" \
@@ -77,3 +68,10 @@ gcloud iam service-accounts keys create "${KEY_FILE}" \
 ./generate_options.py "${PROJECT}" "${BUCKET}" "${KEY_FILE}"
 
 rm "${KEY_FILE}"
+
+### Create Cromwell executions bucket if it doesn't exist
+
+BUCKET=${3:-"${PROJECT}-cromwell-executions"}
+REGION=${4:-"us-east1"}
+
+gsutil mb -l "${REGION}" "gs://${BUCKET}" 2>/dev/null || true
