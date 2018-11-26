273d6e45974ffec7ce4a43c57b90f0066cce835f
Denis Loginov <denis@broadinstitute.org>
2018-11-26 14:13:56 -0500

    Use gcloud.cmd and gsutil.cmd on Windows

--- setup.sh
+++ setup.sh
@@ -6,14 +6,22 @@ set -e
 
 export PATH="${PATH}:${LOCALAPPDATA}/Google/Cloud SDK/google-cloud-sdk/bin"
 
+run_gcloud() {
+  if [ -x "$(command -v gcloud.cmd)" ]; then
+    gcloud.cmd $@
+  else
+    gcloud $@
+  fi
+}
+
 get_project() {
-  gcloud config list --format 'value(core.project)'
+  run_gcloud config list --format 'value(core.project)'
 }
 
 PROJECT=${1:-"$(get_project)"}
 
 enable_api() {
-  gcloud services enable "$1"
+  run_gcloud services enable "$1"
 }
 
 enable_api compute
@@ -25,7 +33,11 @@ BUCKET=${3:-"${PROJECT}-cromwell"}
 REGION=${4:-"us-east1"}
 
 run_gsutil() {
-  gsutil $@ || gsutil.cmd $@
+  if [ -x "$(command -v gsutil.cmd)" ]; then
+    gsutil.cmd $@
+  else
+    gsutil $@
+  fi
 }
 
 run_gsutil mb -l "${REGION}" "gs://${BUCKET}" 2>/dev/null || true
@@ -36,20 +48,20 @@ run_gsutil cp monitoring.sh "gs://${BUCKET}/scripts/"
 # Create SA if it doesn't exist
 
 username() {
-  gcloud config list --format 'value(core.account.split("@").slice(0))'
+  run_gcloud config list --format 'value(core.account.split("@").slice(0))'
 }
 
 SERVICE_ACCOUNT=${2:-"cromwell-$(username)"}
 
 SERVICE_ACCOUNT_EMAIL="${SERVICE_ACCOUNT}@${PROJECT}.iam.gserviceaccount.com"
 
-gcloud iam service-accounts create "${SERVICE_ACCOUNT}" \
+run_gcloud iam service-accounts create "${SERVICE_ACCOUNT}" \
   --display-name "${SERVICE_ACCOUNT}" 2>/dev/null || true
 
 # Add roles and permissions required by Cromwell
 
 add_role() {
-  gcloud projects add-iam-policy-binding "${PROJECT}" \
+  run_gcloud projects add-iam-policy-binding "${PROJECT}" \
     --member "serviceAccount:${SERVICE_ACCOUNT_EMAIL}" \
     --role "roles/$1" >/dev/null
 }
@@ -58,9 +70,9 @@ add_role compute.instanceAdmin.v1
 add_role genomics.pipelinesRunner
 add_role storage.objectAdmin
 
-PROJECT_ID=$(gcloud projects describe "${PROJECT}" --format 'value(projectNumber)')
+PROJECT_ID=$(run_gcloud projects describe "${PROJECT}" --format 'value(projectNumber)')
 
-gcloud iam service-accounts add-iam-policy-binding \
+run_gcloud iam service-accounts add-iam-policy-binding \
   "${PROJECT_ID}-compute@developer.gserviceaccount.com" \
   --member "serviceAccount:${SERVICE_ACCOUNT_EMAIL}" \
   --role "roles/iam.serviceAccountUser" >/dev/null
@@ -68,20 +80,20 @@ gcloud iam service-accounts add-iam-policy-binding \
 # (Re-)generate the key and populate it into options.json
 
 get_keys() {
-  gcloud iam service-accounts keys list \
+  run_gcloud iam service-accounts keys list \
     --iam-account "$1" \
     --managed-by user \
     --format 'value(name)'
 }
 
 for key_id in $(get_keys "${SERVICE_ACCOUNT_EMAIL}"); do
-  gcloud iam service-accounts keys delete "${key_id}" \
+  run_gcloud iam service-accounts keys delete "${key_id}" \
     --iam-account "${SERVICE_ACCOUNT_EMAIL}" -q
 done
 
 KEY_FILE="key.json"
 
-gcloud iam service-accounts keys create "${KEY_FILE}" \
+run_gcloud iam service-accounts keys create "${KEY_FILE}" \
   --iam-account "${SERVICE_ACCOUNT_EMAIL}"
 
 ./generate_options.py "${PROJECT}" "${BUCKET}" "${KEY_FILE}"
@@ -93,4 +105,4 @@ rm "${KEY_FILE}"
 SAM="sam.dsde-prod.broadinstitute.org"
 
 curl -sX POST "https://${SAM}/register/user/v1" \
-   -H "Authorization: Bearer $(gcloud auth print-access-token)" >/dev/null
+   -H "Authorization: Bearer $(run_gcloud auth print-access-token)" >/dev/null
