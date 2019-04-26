8ea8a0873c18e466b52abe5218f004ad44a31ce7
Denis Loginov <denis@broadinstitute.org>
2019-04-26 16:22:07 -0400

    Calculate CPU usage from /proc/stat in monitoring.sh

--- scripts/monitoring.sh
+++ scripts/monitoring.sh
@@ -6,8 +6,6 @@
 # The results should be stored to monitoring.log
 # in the task call's folder.
 
-apt install -y procps &>/dev/null || true
-
 mem() {
   grep ^$1 /proc/meminfo | awk '{ print $2 }'
 }
@@ -18,6 +16,31 @@ mem_total() {
 
 mem_used() {
   echo $(( $(mem_total) - $(mem MemAvailable) ))
+}
+
+cpu_used() {
+  # from https://github.com/Leo-G/DevopsWiki/wiki/How-Linux-CPU-Usage-Time-and-Percentage-is-calculated
+  # by Paul Colby (http://colby.id.au), no rights reserved ;)
+
+  # Get the total CPU statistics, discarding the 'cpu ' prefix.
+  CPU=(`sed -n 's/^cpu\s//p' /proc/stat`)
+  IDLE=${CPU[3]} # Just the idle CPU time.
+
+  # Calculate the total CPU time.
+  TOTAL=0
+  for VALUE in "${CPU[@]}"; do
+    let "TOTAL=$TOTAL+$VALUE"
+  done
+
+  # Calculate the CPU usage since we last checked.
+  let "DIFF_IDLE=$IDLE-$PREV_IDLE"
+  let "DIFF_TOTAL=$TOTAL-$PREV_TOTAL"
+  let "DIFF_USAGE=(1000*($DIFF_TOTAL-$DIFF_IDLE)/$DIFF_TOTAL+5)/10"
+  echo $DIFF_USAGE
+
+  # Remember the total and idle CPU times for the next check.
+  PREV_TOTAL="$TOTAL"
+  PREV_IDLE="$IDLE"
 }
 
 echo ==================================
@@ -33,7 +56,7 @@ echo
 echo --- Runtime Information ---
 echo -e "CPU,%\tMem,%\tDisk\tDate"
 runtimeInfo() {
-  local cpu=$(top -bn 1 | grep -i cpu | grep -o [0-9]* | head -1)
+  local cpu=$(cpu_used)
   local mem=$(( ($(mem_used) * 100) / $(mem_total) ))
   local disk=$(df -h | grep cromwell_root | awk '{ print $3 }')
 
